3b5b7d1eaa4ca7811b904a9226f07bc6c0cd36cd
julietefreitas <juliete.gomes17@gmail.com>
2021-01-26 11:15:18 -0300

    + pasta infra

---
+++ myapp/src/infra/bd.js
@@ -0,0 +1,18 @@
+module.exports =  {
+  usuario: [{
+    "nome":"Juliete",
+    "email":"juli@gmail.com",
+    "senha":"1234",
+  },{
+    "nome":"Andre",
+    "email":"andre@gmail.com",
+    "senha":"123456"},
+    {
+      "nome":"Felipe",
+      "email":"obrabo@gmail.com",
+      "senha":"1234"
+    }],
+    
+  tarefa: [],
+}
+
