dd87623277ee9c355fca5ee87ca69f7b51e0b06d
julietefreitas <juliete.gomes17@gmail.com>
2021-02-10 17:04:20 -0300

    + inserção de routes no app.js

--- myapp/src/app.js
+++ myapp/src/app.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const usuarioControler = require('./controllers/usuario-controller');
-const tarefaControler = require ('./controllers/tarefa-controller');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
 const port = 8000;
-
 
 app.use(bodyParser.json());
 app.use(cors());
 
-usuarioControler(app);
-tarefaControler(app);
+routes(app);
 
 app.listen(port, () => {
   console.log(`Servidor rodando no endereço http://localhost:${port}`);
-});  
+});
