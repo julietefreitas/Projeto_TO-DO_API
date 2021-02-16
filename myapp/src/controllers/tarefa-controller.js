47075895d01373ca5db6f565fc5d181b72413874
julietefreitas <juliete.gomes17@gmail.com>
2021-02-16 00:13:00 -0300

    + funções rotas get e getById

--- myapp/src/controllers/tarefa-controller.js
+++ myapp/src/controllers/tarefa-controller.js
@@ -1,5 +1,9 @@
 const Tarefa = require('../models/model-tarefa');
-
+const TarefaDAO = require('../DAO/tarefa-dao');
+const bd = require("../infra/sqlite-db");
+const TarefaBD = new TarefaDAO(bd);
+const { validationResult } = require("express-validator");
+/* 
 module.exports = (app, bd) => {
   app.get('/tarefa', (req, resp) => {
     console.log('Rota ativada com GET e recurso retornando um objeto JSON');
@@ -12,5 +16,33 @@ module.exports = (app, bd) => {
     bd.tarefa.push(tarefa);
   })
 }
+ */
+ class ControllerTarefa {
+   
+  static listarTarefas(){
+    return async (req, resp) => {
+      const listaDeTarefas = await TarefaBD.listaTarefas()
+      .then((listaDeTarefas) => {
+        resp.send(listaDeTarefas);
+      })
+      .catch((error) =>{
+        resp.send(error);
+      });
+    }  
+  }
 
+  static listarTarefasById() {
+    return async (req, resp) => {
+      const id = req.params.id;
+      const listaTarefaById = await TarefaBD.pesquisaById(id)
+      .then((tarefa) => {
+        resp.send(tarefa);
+      })
+      .catch((error) =>{
+        resp.send(error);
+      }); 
+    }
+  }
 
+}
+ module.exports = ControllerTarefa;
