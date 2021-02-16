13806aaf61d6ee21289334a9fc6d5191668c0d46
julietefreitas <juliete.gomes17@gmail.com>
2021-02-16 00:09:11 -0300

    + rotas get e getbyId da entidade tarefa

--- myapp/src/routes/routes.js
+++ myapp/src/routes/routes.js
@@ -1,16 +1,22 @@
-const Controller = require('../controllers/usuario-controller');
+const ControllerUsuario = require('../controllers/usuario-controller');
+const ControllerTarefa = require('../controllers/tarefa-controller');
 const {nome, email} = require('../models/model-usuario');
-const {nome,senha} = require('../models/model-usuario');
+//const {nome,senha} = require('../models/model-usuario');
 
 module.exports = (app) => {
 
-  app.get('/usuario', Controller.listarUsuarios());
+  app.get('/usuario', ControllerUsuario.listarUsuarios());
 
-  app.get('/usuario/:id' , Controller.listarUsuariosById());
+  app.get('/usuario/:id' , ControllerUsuario.listarUsuariosById());
 
-  app.post('/usuario', [nome, email], Controller.inserirUsuario());
+  app.post('/usuario', [nome, email], ControllerUsuario.inserirUsuario());
 
-  app.delete('/usuario/:id',Controller.deletarUsuario());
+  app.delete('/usuario/:id', ControllerUsuario.deletarUsuario());
 
-  app.put('/usuario:/id',[nome,senha], Controller.modificaUsuario());
+ // app.put('/usuario:/id',[nome,senha], ControllerUsuario.modificaUsuario());
+
+  app.get('/tarefa', ControllerTarefa.listarTarefas());
+
+  app.get('/tarefa/:id', ControllerTarefa.listarTarefasById());
+
 }
