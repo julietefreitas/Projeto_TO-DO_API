603b7bae45074ac4c33a6979d6f91465f50367d1
julietefreitas <juliete.gomes17@gmail.com>
2021-02-16 12:30:17 -0300

    + verificação dos tipos dos campos entidade tarefa

--- myapp/src/models/model-tarefa.js
+++ myapp/src/models/model-tarefa.js
@@ -1,10 +1,22 @@
-class modelTarefa {
-  constructor(titulo,descricao,status,dataDeCriacao) {
-    this.titulo = titulo;
-    this.descricao = descricao;
-    this.status = status;
-    this.dataDeCriacao = dataDeCriacao;
-  }
-}
+const {body} = require('express-validator');
 
-module.exports = modelTarefa;
+module.exports = {
+  titulo: body("TITULO")
+    .isLength({ min: 10 })
+    .withMessage(`Titulo de tarefa menor que o permitido`)
+    .isLength({ max: 64 })
+    .withMessage(`Titulo de tarefa maior que o permitido`)
+    .isString()
+    .withMessage(`Titulo com caracteres não permitidos`),
+  descricao: body("DESCRICAO")
+    .isLength({ min: 10 })
+    .withMessage(`Descrição menor que o tamanho mínimo`)
+    .isLength({ max: 32 })
+    .withMessage(`Descrição maior que o tamanho permitido`),
+  status: body("STATUS")
+    .equals("TODO","DOING","CONTÍNUO")
+    .withMessage(`Status fora das possibilidades`),
+  datacriacao: body("DATACRIACAO")
+    .isDate({ format: "YYYY-MM-DD" })
+    .withMessage(`Data no formato incorreto`),
+};
