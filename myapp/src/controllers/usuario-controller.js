261e21c4347159b02b511294e9f20c8431a72868
julietefreitas <juliete.gomes17@gmail.com>
2021-02-16 15:33:53 -0300

    + rota de put da entidade usuário

--- myapp/src/controllers/usuario-controller.js
+++ myapp/src/controllers/usuario-controller.js
@@ -1,95 +1,74 @@
-const UsuariosDAO = require ('../DAO/usuario-dao');
-const bd = require('../infra/sqlite-db');
-const usuario = new UsuariosDAO(bd);
+const UsuariosDAO = require("../DAO/usuario-dao");
+const bd = require("../infra/sqlite-db");
+const UsuarioBD = new UsuariosDAO(bd);
+const { validationResult } = require("express-validator");
 
-module.exports = (app) => {
-
-
-  app.get('/usuario', async (req, resp) => {
-    const listaDeUsuarios = await usuario.listaUsuarios();
-    resp.send(listaDeUsuarios);
-  });
-
-  app.get('/usuario/:id', async (req, resp) => {
-    const id = req.params.id;
-    const usuarioPesquisado =  await usuario.pesquisaByEmail(id);
-    resp.send(usuarioPesquisado);
-  });
-
-  app.post('/usuario' , async (req,resp) => {
-    const newUsuario = req.body;
-    const resultadoAssincrono = await usuario.insereUsuario(newUsuario);
-    resp.send(resultadoAssincrono);
-  });
-
-
-  app.delete('/usuario/:id', async (req, resp) => {
-    const id = req.params.id;
-    const usuarioDeletado = await usuario.deletaUsuario(id)
-    .then (usuarioDeletado => {
-      resp.send(usuarioDeletado);
-    })
-    .catch(error => {
-      resp.send(error);
-    });
-  });
-
-  app.put('/usuario/:id', async (req,resp) => {
-    const id = req.params.id;
-    const updateUsuario = await usuario.updateUsuario(id,req.body)
-    .then(updateUsuario => {
-      resp.send(updateUsuario);
-    })
-    .catch(error => {
-      resp.send(error);
-    });
-  });
-
-
+class ControllerUsuario {
+  static listarUsuarios() {
+    return async (req, resp) => {
+      const listaDeUsuarios = await UsuarioBD.listaUsuarios();
+      resp.send(listaDeUsuarios);
+    };
   }
 
+  static listarUsuariosById() {
+    return async (req, resp) => {
+      const id = req.params.id;
+      const listaUsuarioById = await UsuarioBD.pesquisaById(id)
+        .then((listaDeUsuarios) => {
+          resp.send(listaDeUsuarios);
+        })
+        .catch((error) => {
+          resp.send(error);
+        });
+    };
+  }
 
+  static inserirUsuario() {
+    return async (req, resp) => {
+      const erros = validationResult(req);
+      console.log(erros);
+      if (!erros.isEmpty()) {
+        resp.send(`Parâmetros incorretos!<h3>Usuário não inserido</h3>`);
+      } else {
+        const newUsuario = req.body;
+        const resultadoAssincrono = await UsuarioBD.insereUsuario(newUsuario);
+        resp.send(resultadoAssincrono);
+      }
+    };
+  }
 
+  static deletarUsuario() {
+    return async (req, resp) => {
+      const id = req.params.id;
+      const usuarioDeletado = await UsuarioBD.deletaUsuario(id)
+        .then((usuarioDeletado) => {
+          resp.send(usuarioDeletado);
+        })
+        .catch((error) => {
+          resp.send(error);
+        });
+    };
+  }
 
+  static alterarUsuario(){
+    return async(req,resp) => {
+      const id = req.params.id;
+      const erros = validationResult(req);
+      if(!erros.isEmpty()){
+        resp.send(`Parâmetros incorretos!<h3>${erros.errors[0].msg}</h3>`);
+      }else if (req.body.EMAIL != undefined || req.body.ID != undefined){
+        resp.send(`Só é possível alterar NOME E SENHA do seu usuário!`);
+      }
+      else {
+        const body = req.body;
+        const resultadoAssincrono = await UsuarioBD.updateUsuario(id, body);
+        resp.send(resultadoAssincrono);
+      }
+    }
+  }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function preencherBanco() {
-  return {
-    "nome":"Juliete",
-    "email":"juli@gmail.com",
-    "senha":"1234",
-  },{
-    "nome":"Andre",
-    "email":"andre@gmail.com",
-    "senha":"123456"}
-  ;
-} */
-
-
-
-
+module.exports = ControllerUsuario;
 
