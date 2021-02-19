325e31f614a31cc0765edb3da3ddfac98f7a6bfa
julietefreitas <juliete.gomes17@gmail.com>
2021-02-18 23:58:19 -0300

    + try catch controller usuario

--- myapp/src/DAO/usuario-dao.js
+++ myapp/src/DAO/usuario-dao.js
@@ -4,54 +4,73 @@ class UsuariosDAO {
   }
 
   listaUsuarios() {
-    return new Promise ((resolve,reject) => {
-      this.bancoDeDados.all(`SELECT * FROM USUARIOS`, (error, linhasSelecionadas) => {
-        if(error)reject(error)
-        else(resolve(linhasSelecionadas));
-      }); 
-    });
-  }
-
-  pesquisaByEmail(id) {
-    return new Promise ((resolve,reject) =>{
-      this.bancoDeDados.get(`SELECT * FROM USUARIOS WHERE ID = ?`,[id], (error, resultadoPesquisa) => {
-        if(error)reject(error)
-        else(resolve(resultadoPesquisa));
-      });
-    });
-  }
-
-   insereUsuario(newUsuario) {
-    return new Promise ((resolve, reject) => {
-      this.bancoDeDados.run(`INSERT INTO USUARIOS (NOME,EMAIL,SENHA) VALUES (?,?,?)`, [newUsuario.NOME, newUsuario.EMAIL, newUsuario.SENHA], (error) => {
-        if(error)reject(`Usuário não inserido!`);
-        else resolve("Usuário inserido com sucesso!");
-      });
-    });
-  }
-
-  deletaUsuario(id){
-    return new Promise ((resolve, reject) => {
-      this.bancoDeDados.run(`DELETE FROM USUARIOS WHERE ID = ?`, [id], function (error) {
-        if(error)reject(error);
-        if(this.changes == 0) {
-          reject("Nenhum usuário com esse identificador foi encontrado!");
+    return new Promise((resolve, reject) => {
+      this.bancoDeDados.all(
+        `SELECT * FROM USUARIOS`,
+        (error, linhasSelecionadas) => {
+          if (error) reject(error);
+          else resolve(linhasSelecionadas);
         }
-        else resolve("Usuário deletado com sucesso!");
-      });
+      );
     });
   }
 
-  updateUsuario(id, body){
+  pesquisaById(id) {
+    return new Promise((resolve, reject) => {
+      this.bancoDeDados.get(
+        `SELECT * FROM USUARIOS WHERE ID = ?`,
+        [id],
+        (error, resultadoPesquisa) => {
+          if (error)reject(error);
+          else if (resultadoPesquisa == undefined) {
+            reject(`Usuário não encontrado no banco!`);
+          } else resolve(resultadoPesquisa);
+        }
+      );
+    });
+  }
+
+  insereUsuario(newUsuario) {
+    return new Promise((resolve, reject) => {
+      this.bancoDeDados.run(
+        `INSERT INTO USUARIOS (NOME,EMAIL,SENHA) VALUES (?,?,?)`,
+        [newUsuario.NOME, newUsuario.EMAIL, newUsuario.SENHA],
+        (error) => {
+          if(error) reject(`Usuário não inserido!`);
+          else resolve("Usuário inserido com sucesso!");
+        }
+      );
+    });
+  }
+
+  deletaUsuario(id) {
+    return new Promise((resolve, reject) => {
+      this.bancoDeDados.run(
+        `DELETE FROM USUARIOS WHERE ID = ?`,
+        [id],
+        function (error) {
+          if (error) reject(error);
+          if (this.changes == 0) {
+            reject("Nenhum usuário com esse identificador foi encontrado!");
+          } else resolve("Usuário deletado com sucesso!");
+        }
+      );
+    });
+  }
+
+  updateUsuario(id, body) {
     console.log(body);
-    return new Promise ((resolve, reject) => {
-      this.bancoDeDados.run(`UPDATE USUARIOS SET NOME=?, EMAIL=?,SENHA=? WHERE ID=?`, [body.NOME,body.EMAIL,body.SENHA, id], (error) => {
-        if(error)reject(error);
-        if(this.changes == 0) {
-          reject("Nenhum usuário com esse identificador foi encontrado!");
+    return new Promise((resolve, reject) => {
+      this.bancoDeDados.run(
+        `UPDATE USUARIOS SET NOME=?,SENHA=? WHERE ID=?`,
+        [body.NOME,body.SENHA, id],
+        (error) => {
+          if (error) reject(error);
+          if (this.changes == 0) {
+            reject("Nenhum usuário com esse identificador foi encontrado!");
+          } else resolve(`Alteração feita com sucesso!`);
         }
-        else resolve(`Alteração feita com sucesso!`);
-      });
+      );
     });
   }
 }
--- myapp/src/controllers/usuario-controller.js
+++ myapp/src/controllers/usuario-controller.js
@@ -6,34 +6,43 @@ const { validationResult } = require("express-validator");
 class ControllerUsuario {
   static listarUsuarios() {
     return async (req, resp) => {
-      const listaDeUsuarios = await UsuarioBD.listaUsuarios();
-      resp.send(listaDeUsuarios);
-    };
+      try{
+        const listaDeUsuarios = await UsuarioBD.listaUsuarios();
+        resp.status(200).send(listaDeUsuarios);
+      }
+      catch(err){
+        resp.send(err);
+      }
+    }
   }
 
   static listarUsuariosById() {
     return async (req, resp) => {
       const id = req.params.id;
-      const listaUsuarioById = await UsuarioBD.pesquisaById(id)
-        .then((listaDeUsuarios) => {
-          resp.send(listaDeUsuarios);
-        })
-        .catch((error) => {
-          resp.send(error);
-        });
+      try{
+        const listaUsuarioById = await UsuarioBD.pesquisaById(id);
+        resp.send(listaUsuarioById);
+      }
+      catch (err){
+          resp.status(200).send(err);
+        }
     };
   }
 
   static inserirUsuario() {
     return async (req, resp) => {
       const erros = validationResult(req);
-      console.log(erros);
       if (!erros.isEmpty()) {
-        resp.send(`Parâmetros incorretos!<h3>Usuário não inserido</h3>`);
+        resp.status(200).send(`Parâmetros incorretos!<h3>${erros.errors[0].msg}!</h3>Usuário não inserido!`);
       } else {
-        const newUsuario = req.body;
-        const resultadoAssincrono = await UsuarioBD.insereUsuario(newUsuario);
-        resp.send(resultadoAssincrono);
+        try{
+          const newUsuario = req.body;
+          const resultadoAssincrono = await UsuarioBD.insereUsuario(newUsuario);
+          resp.send(resultadoAssincrono);
+        }
+        catch(err){
+          resp.send(err)
+        }
       }
     };
   }
@@ -41,13 +50,13 @@ class ControllerUsuario {
   static deletarUsuario() {
     return async (req, resp) => {
       const id = req.params.id;
-      const usuarioDeletado = await UsuarioBD.deletaUsuario(id)
-        .then((usuarioDeletado) => {
-          resp.send(usuarioDeletado);
-        })
-        .catch((error) => {
-          resp.send(error);
-        });
+      try{
+        const usuarioDeletado = await UsuarioBD.deletaUsuario(id)
+        resp.send(usuarioDeletado);
+      }
+      catch(err){
+         resp.send(err);
+      }
     };
   }
 
@@ -61,9 +70,14 @@ class ControllerUsuario {
         resp.send(`Só é possível alterar NOME E SENHA do seu usuário!`);
       }
       else {
-        const body = req.body;
-        const resultadoAssincrono = await UsuarioBD.updateUsuario(id, body);
-        resp.send(resultadoAssincrono);
+        try {
+          const body = req.body;
+          const resultadoAssincrono = await UsuarioBD.updateUsuario(id, body);
+          resp.send(resultadoAssincrono);
+        }
+        catch(err){
+          resp.send(err);
+        }
       }
     }
   }
